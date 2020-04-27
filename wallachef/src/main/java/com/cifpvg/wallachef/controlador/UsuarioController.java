bdd9718550c4b7c7ee592865dcc33f047db78b13
CarlosDez23 <cgonzalezd10@hotmail.com>
2020-04-27 14:10:00 +0200

    Login funcionando

--- wallachef/src/main/java/com/cifpvg/wallachef/controlador/UsuarioController.java
+++ wallachef/src/main/java/com/cifpvg/wallachef/controlador/UsuarioController.java
@@ -1,10 +1,13 @@
 package com.cifpvg.wallachef.controlador;
 
+import java.util.Optional;
+
 import com.cifpvg.wallachef.dao.UsuarioDao;
 import com.cifpvg.wallachef.modelo.Usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,15 +29,35 @@ public class UsuarioController {
     }
 
     /**
-     * Método para insertar un usuario
-     * @param param
-     * @return
+     * Método para insertar un usuario. Lo utilizaremos para registrar un usuairo
+     * @param usuario
+     * @return ResponseEntity --> El usuario insertado si ha podido insertarlo con éxito
      */
     @RequestMapping(value="nuevo", method=RequestMethod.POST)
     public ResponseEntity<Usuario> insertarUsuario(@RequestBody Usuario usuario) {
         Usuario u = daoUsuario.save(usuario);
         return ResponseEntity.ok(u);
     }
+
+    /**
+     * Método para comprobar si existe un usuario con un nombre y una contraseña.
+     * Lo utilizaremos para la parte del login
+     * @param nombre
+     * @param password
+     * @return ResponseEntity --> Retornará el usuario si existe y si no código de error (204)
+     */
+    @RequestMapping(value="login/{nombre}/{password}", method=RequestMethod.GET)
+    public ResponseEntity<Usuario> comprobarLogin(@PathVariable("nombre") String nombre, @PathVariable("password") String password) {
+        Optional<Usuario> optionalUsuario = daoUsuario.findByNombreAndPassword(nombre,password);
+        if (optionalUsuario.isPresent()){
+            return ResponseEntity.ok(optionalUsuario.get());
+        }else{
+            return ResponseEntity.noContent().build();
+        }
+    }
+    
+
+
     
     
 
--- wallachef/src/main/java/com/cifpvg/wallachef/dao/UsuarioDao.java
+++ wallachef/src/main/java/com/cifpvg/wallachef/dao/UsuarioDao.java
@@ -1,9 +1,13 @@
 package com.cifpvg.wallachef.dao;
 
+import java.util.Optional;
+
 import com.cifpvg.wallachef.modelo.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Integer> {
+
+	Optional<Usuario> findByNombreAndPassword(String nombre, String password);
 
 }
